a840d6b7f3ab4adcd9d7130556555f2f0aab78f4
yonas samuel <yonastlolo@yahoo.com>
2017-01-24 15:29:35 -0500

    done

--- crm.rb
+++ crm.rb
@@ -1,3 +1,31 @@
 # Implement the new web-based CRM here.
 # Do NOT copy the CRM class from the old crm assignment, as it won't work at all for the web-based version!
 # You'll have to implement it from scratch.
+require_relative 'contact'
+require 'sinatra'
+"/contacts/new"
+# get '/'do
+#   'erb :index'
+# end
+# Fake data
+# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+# Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+# Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
+  get '/' do
+   @crm_app_name = "Yonas CRM"
+   erb :index
+  end
+
+  get'/contacts' do
+    erb :contacts
+  end
+
+  get '/contacts/new' do
+     erb :new_contact
+  end
+
+  post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
+end
